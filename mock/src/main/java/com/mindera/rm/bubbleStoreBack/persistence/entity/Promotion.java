c76163712c2ae16220eb8a048e93ce17445aef11
Rafael Martins <41962783+rmzaoo@users.noreply.github.com>
2022-05-02 23:01:38 +0100

    working

--- mock/src/main/java/com/mindera/rm/bubbleStoreBack/persistence/entity/Promotion.java
+++ mock/src/main/java/com/mindera/rm/bubbleStoreBack/persistence/entity/Promotion.java
@@ -1,13 +1,15 @@
 package com.mindera.rm.bubbleStoreBack.persistence.entity;
 
 import com.mindera.rm.bubbleStoreBack.enumerators.PromotionType;
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
 import java.math.BigDecimal;
 
 @Data
+@ToString
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Promotion {
     private String id;
     private PromotionType type;
